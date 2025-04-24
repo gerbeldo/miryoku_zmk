dcaeeea6f7293b463f4b5c45b90622e8e4fa1212
german <gerbeldo@gmail.com>
2025-04-23 22:54:30 -0400

    point to flake-zmk-module for shield config

--- miryoku/mapping/46/anywhy_flake.h
+++ miryoku/mapping/46/anywhy_flake.h
@@ -1,11 +1,11 @@
 // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#if !defined (MIRYOKU_LAYOUTMAPPING_FLAKEM)
+#if !defined (MIRYOKU_LAYOUTMAPPING_ANYWHY_FLAKE)
 
 #define XXX &none
 
-#define MIRYOKU_LAYOUTMAPPING_FLAKEM( \
+#define MIRYOKU_LAYOUTMAPPING_ANYWHY_FLAKE( \
      K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
@@ -18,4 +18,4 @@ XXX  K20  K21  K22  K23  K24                 K25  K26  K27  K28  K29  XXX \
 
 #endif
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_FLAKEM
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_ANYWHY_FLAKE
